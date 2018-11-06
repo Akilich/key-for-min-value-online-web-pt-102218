81b56adf34be2055410cc3241abbb326c6e492ef
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 05:24:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,6 @@ name_hash.each do |name, number|
     key_for_min_value = name
   elsif number.to_i > name_hash[key_for_min_value].to_i
     key_for_min_value = name
-    return key_for_min_value
 end
 end
 end
