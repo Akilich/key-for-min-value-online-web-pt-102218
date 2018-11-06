5e076e6b7e4e997820daa1665b3d32e274c197bd
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 04:50:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name = " "
 name_hash.each do |name, number|
   if key_for_min_value == nil
-    key_for_min_value = 
+    key_for_min_value = name
         return name
   else name_hash = [ ]
     return nil
