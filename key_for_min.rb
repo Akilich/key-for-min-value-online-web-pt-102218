22199cf18ea354fa5279d18f1836d89532afd011
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 03:23:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 name_hash.each do |name, number|
-    if number == :1 && name.start_with?
+    if number == :1 && name.start_with?("a")
       return adam
 end
 end
